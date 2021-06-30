9d6bde9dccd7c6feb28a18fc506931cb02150cb7
Matse007 <matthiastobin@web.de>
2021-06-30 12:11:04 +0200

    commented out showmeme, as its not finished yet

--- commands/fun/showmemes.js
+++ commands/fun/showmemes.js
@@ -1,12 +1,12 @@
 let memes;
 var fs = require("fs");
-//TODO: Make an embedd with all memes
+//TODO: Make an embedd with all memes?
 module.exports = {
   name: "showmeme",
   description: "this is a meme command!",
   guildOnly: true,
   execute(message, args) {
-    if (
+    /* if (
       message.member.roles.cache.find((role) => (role.name = "Admins")) ||
       message.author.id == "72182588885700608" || message.author.id == "82799177439907840"
     ) 
@@ -28,6 +28,6 @@ module.exports = {
           }
         }
       );
-    
+     */
   },
 };
