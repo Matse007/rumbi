4ae8192a72f7f1b394049980116d25f1f418b248
Matse007 <matthiastobin@web.de>
2021-07-01 16:28:09 +0200

    Fixed the ready event and set the status properly

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,11 @@
 //bot logged in successfully and it's ready to be used
-exports = {
+module.exports = {
 	name: 'ready',
 	once: true, 
 	execute(client) {
         client.user.setPresence({
-            game: {
-                name: 'in Hat Kid\'s Spaceship',
-                type: 0
+            activity: {
+                name: '$help for more info',
             }
         });
       console.log(`Ready to  server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
